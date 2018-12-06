f5c4231022f7efc638b20cbd8bfbfd48253e19ff
Filip Isaksson <FilipIsaksson@hotmail.com>
2018-12-06 21:22:39 +0100

    added list of foods

--- frontend/src/app/components/list/list.component.ts
+++ frontend/src/app/components/list/list.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  today: number = Date.now();
+
+
+  food_list = [
+    {title: 'Sukaldari', rating: 4.5},
+    {title: 'Shawarma', rating: 4.3},
+    {title: 'Döner', rating: 4.2},
+    {title: 'Pandori', rating: 4.1},
+    {title: 'Falafel', rating: 3.8},
+    {title: 'Hamburgare', rating: 3.2},
+    {title: 'Kebab', rating: 3.1},
+    {title: 'Grillen', rating: 2.9},
+  ];
+
   constructor() { }
 
   ngOnInit() {
