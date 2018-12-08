1a43d32f6601a05457c07d124863576c9e110de2
Filip Isaksson <FilipIsaksson@hotmail.com>
2018-12-08 14:10:28 +0100

    learning mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,44 @@
-const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mydb'); //:27017
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+var myFirstSchema = new mongoose.Schema(
+  { name: String }
+  );
+
+var myModel = mongoose.model('myModel', myFirstSchema);
+
+var blum = new myModel({name: 'blumName'});
+console.log('blum: ');
+console.log(blum.name);
+
+myFirstSchema.methods.ingred = function () {
+  var greeting = this.name
+  ? "Meow name is " + this.name
+  : "I don't have a name";
+  console.log(greeting);
+}
+
+var flip = mongoose.model('myFlipModel', myFirstSchema); //collection
+
+var _instance = new flip({name: 'Fluffy'}); //instance of document-schema
+_instance.ingred();
+
+_instance.save( (err, _instance) => {
+  if (err) console.error(err);
+  _instance.ingred();
 });
 
-app.get('/node', (req, res) => {
-  res.send([1,3,4]);
-})
+console.log('rword');
+flip.find( (err, rword) => {
+  if (err) console.error(err);
+  console.log(rword);
+});
 
 
-app.listen(3000, () => {
-  console.log('listening on port 3000 ...');
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  console.log('we are connected');
 });
