368bbda2ac502ad376001b405c3455f2b47a9126
Filip Isaksson <FilipIsaksson@hotmail.com>
2018-12-17 14:28:05 +0100

    added server to retrive top 10 rated from mongo

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const server = express();
 const mongoose = require('mongoose');
-const myFoodModel = require('food.js');
+const myFoodModel = require('./food.js');
 
 mongoose.Promise = Promise; // SWitch from callback to promise
 mongoose.connect('mongodb://localhost/mydb').then((err) => {
@@ -10,6 +10,17 @@ mongoose.connect('mongodb://localhost/mydb').then((err) => {
 }); //:27017
 
 server.use(bodyParser.json());
+
+server.post('/p', async (req, res) => {
+  //const {name, rating} = req.body;
+  const resp = await myFoodModel.find().sort({'rating':-1}).limit(10); // find fodd top 10 rating
+  if (!resp) {
+    console.log('Error not found');
+  }else{
+    res.json(resp); // returning object of foods
+  }
+  //res.send(resp);
+})
 
 server.listen(3000, () => {
   console.log('Server running on port 3000');
